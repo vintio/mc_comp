94bce3f9cb2b95627ec8df0053ed19a71d04cd34
vintio <vin_tio@mail.ru>
2024-02-11 19:41:25 +0300

    upd

--- Downloader.app/Main.lua
+++ Downloader.app/Main.lua
@@ -7,13 +7,11 @@ local event = require("Event")
 local json = require("JSON")
 local internet = require("Internet")
 
-local localization = system.getCurrentScriptLocalization()
-
 local internet = nil
 if component.isAvailable("internet") then
   internet = component.internet
 else
-  GUI.alert(localization.notInternet)
+  GUI.alert("No internet card")
   return
 end
 
@@ -24,7 +22,7 @@ local url = window:addChild(GUI.input(5, 5, 30, 3, 0x1E1E1E, 0xA5A5A5, 0xA5A5A5,
 --local path = window:addChild(GUI.filesystemChooser(5, 13, 30, 3, 0x1E1E1E, 0xA5A5A5, 0x1E1E1E, 0xA5A5A5, localization.choosePath, localization.choose, localization.cancel, localization.choosePath, "/"))
 local file = window:addChild(GUI.input(5, 9, 30, 3, 0x1E1E1E, 0xA5A5A5, 0xA5A5A5, 0x1E1E1E, 0xFFFFFF, "/Projects/", "Local", false))
 --path:setMode(GUI.IO_MODE_DIRECTORY)
-local downloadButton = window:addChild(GUI.button(5, 17, 30, 3, 0x1E1E1E, 0xA5A5A5, 0xA5A5A5, 0x1E1E1E, localization.download))
+local downloadButton = window:addChild(GUI.button(5, 17, 30, 3, 0x1E1E1E, 0xA5A5A5, 0xA5A5A5, 0x1E1E1E, "Download"))
 
 window:addChild(GUI.text(1, window.height, 0xA5A5A5, "GitHub Dowloader 0.1.5"))
 
